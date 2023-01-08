c1d8fd1c42be9bb027b50f18fa31ea8179d865b9
Grechkin Danil <grechkin-danil@mail.ru>
2023-01-08 11:32:10 +0200

    basic work functional programming redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+function createStore(initialState) {
+  let state = initialState;
+  function getState() {
+    return state;
+  }
+  function dispatch(action) {
+    console.log(action);
+    if (action.type === 'task/completed') {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex].completed = true;
+      state = newArray;
+      console.log(state);
+    }
+  }
+  return { getState, dispatch };
+}
+const store = createStore([{ id: 1, description: 'Task 1', completed: false }]);
 function App() {
+  console.log(store.getState());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      App
+      <button
+        onClick={() => {
+          store.dispatch({ type: 'task/completed', payload: { id: 1 } });
+        }}
+      >
+        Complete
+      </button>
     </div>
   );
 }
