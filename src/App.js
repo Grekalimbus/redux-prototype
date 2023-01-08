a66198da4013b6249342220f44e4c66d764a0df5
Grechkin Danil <grechkin-danil@mail.ru>
2023-01-08 12:52:08 +0200

    add new functional and add observer pattern

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Reducer - функция, которая принимает состояние, action, и далее в зависимости от action.type через switch реазиловываем действие
 function taskReducer(state, action) {
   switch (action.type) {
-    case 'task/completed':
-      const newArray = [...state];
-      const elementIndex = newArray.findIndex(
-        (el) => el.id === action.payload.id
-      );
-      newArray[elementIndex].completed = true;
-      return newArray;
+    case 'task/updated':
+      {
+        const newArray = [...state];
+        const elementIndex = newArray.findIndex(
+          (el) => el.id === action.payload.id
+        );
+        newArray[elementIndex] = {
+          ...newArray[elementIndex],
+          ...action.payload,
+        };
+        return newArray;
+      }
       break;
     default:
       break;
@@ -19,25 +24,46 @@ function taskReducer(state, action) {
 function createStore(reducer, initialState) {
   // state - состояние
   let state = initialState;
+  let listeners = [];
   function getState() {
     return state;
   }
   // функция, меняющая состояние
   function dispatch(action) {
     state = reducer(state, action);
+    for (let i = 0; i < listeners.length; i++) {
+      const listener = listeners[i];
+      listener();
+    }
   }
-  return { getState, dispatch };
+  function subscribe(listener) {
+    listeners.push(listener);
+  }
+  return { getState, dispatch, subscribe };
 }
 // переменная, где в createStore пушится изначальное состояние
 const store = createStore(taskReducer, [
-  { id: 1, description: 'Task 1', completed: false },
-  { id: 2, description: 'Task 2', completed: false },
+  { id: 1, title: 'Task 1', completed: false },
+  { id: 2, title: 'Task 2', completed: false },
 ]);
 function App() {
-  const state = store.getState();
+  const [state, setState] = useState(store.getState());
+  useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState());
+    });
+  }, []);
   const completeTask = (taskID) => {
-    store.dispatch({ type: 'task/completed', payload: { id: taskID } });
-    console.log(store.getState());
+    store.dispatch({
+      type: 'task/updated',
+      payload: { id: taskID, completed: true },
+    });
+  };
+  const changeTitle = (taskID) => {
+    store.dispatch({
+      type: 'task/updated',
+      payload: { id: taskID, title: `New title for ${taskID}` },
+    });
   };
 
   return (
@@ -47,7 +73,7 @@ function App() {
         {state.map((item) => {
           return (
             <li key={item.id}>
-              <p>{` ${item.description}`}</p>
+              <p>{` ${item.title}`}</p>
               <p>{`completed: ${item.completed}`}</p>
               <button
                 onClick={() => {
@@ -55,6 +81,13 @@ function App() {
                 }}
               >
                 Complete
+              </button>
+              <button
+                onClick={() => {
+                  changeTitle(item.id);
+                }}
+              >
+                Change title
               </button>
               <hr />
             </li>
