e0ac27e23ec31f9cbc51d0e5d4a2cae2d10ad17c
Grechkin Danil <grechkin-danil@mail.ru>
2023-01-10 13:59:07 +0200

    add createReducer for task reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,6 @@
 import { legacy_createStore as configureStore } from 'redux';
 import taskReducer from './task';
 
-const initialState = [
-  { id: 1, title: 'Task 1', completed: false },
-  { id: 2, title: 'Task 2', completed: false },
-];
-
 export function initiateState() {
-  return configureStore(taskReducer, initialState);
+  return configureStore(taskReducer);
 }
