d6768f48972207f50715211a6178565399f19bc1
Grechkin Danil <grechkin-danil@mail.ru>
2023-01-10 14:20:24 +0200

    add redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -2,5 +2,8 @@ import { legacy_createStore as configureStore } from 'redux';
 import taskReducer from './task';
 
 export function initiateState() {
-  return configureStore(taskReducer);
+  return configureStore(
+    taskReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 }
