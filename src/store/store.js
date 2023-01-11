a6a337f18462d7fd3edd3f33514361c78965af9c
Grechkin Danil <grechkin-danil@mail.ru>
2023-01-11 20:32:37 +0200

    add moddleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,20 @@
-import { legacy_createStore as configureStore } from 'redux';
+import {
+  legacy_createStore as configureStore,
+  compose,
+  applyMiddleware,
+} from 'redux';
+import { logger } from './middleware/logger';
 import taskReducer from './task';
+
+const middlewareEnhancer = applyMiddleware(logger);
 
 export function initiateState() {
   return configureStore(
     taskReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(
+      middlewareEnhancer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   );
 }
