c4dfb77b85a60ac715697ba7ae534bd0e8775fe6
Grechkin Danil <grechkin-danil@mail.ru>
2023-01-15 04:33:03 +0200

    initial react-redux and add Provider in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Provider } from 'react-redux';
 import { createStore } from './store/store';
 import {
   titleChanged,
@@ -24,44 +25,46 @@ function App() {
   };
 
   return (
-    <div>
-      App
-      <ul>
-        {state.map((item) => {
-          return (
-            <li key={item.id}>
-              <p>{` ${item.title}`}</p>
-              <p>{`completed: ${item.completed}`}</p>
-              <button
-                className="btn btn-primary m-2"
-                onClick={() => {
-                  store.dispatch(completeTask(item.id));
-                }}
-              >
-                Complete
-              </button>
-              <button
-                className="btn btn-success m-2"
-                onClick={() => {
-                  changeTitle(item.id);
-                }}
-              >
-                Change title
-              </button>
-              <button
-                className="btn btn-danger m-2"
-                onClick={() => {
-                  deleteTask(item.id);
-                }}
-              >
-                Delete task
-              </button>
-              <hr />
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <Provider store={store}>
+      <div>
+        App
+        <ul>
+          {state.map((item) => {
+            return (
+              <li key={item.id}>
+                <p>{` ${item.title}`}</p>
+                <p>{`completed: ${item.completed}`}</p>
+                <button
+                  className="btn btn-primary m-2"
+                  onClick={() => {
+                    store.dispatch(completeTask(item.id));
+                  }}
+                >
+                  Complete
+                </button>
+                <button
+                  className="btn btn-success m-2"
+                  onClick={() => {
+                    changeTitle(item.id);
+                  }}
+                >
+                  Change title
+                </button>
+                <button
+                  className="btn btn-danger m-2"
+                  onClick={() => {
+                    deleteTask(item.id);
+                  }}
+                >
+                  Delete task
+                </button>
+                <hr />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </Provider>
   );
 }
 
