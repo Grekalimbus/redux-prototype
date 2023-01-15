31c332c2b5cf4c299de83731503490fb63e3ffef
Grechkin Danil <grechkin-danil@mail.ru>
2023-01-15 23:15:39 +0200

    add isLoading. modified actions taskRequested and taskRequestFailed

--- src/index.js
+++ src/index.js
@@ -10,8 +10,11 @@ import {
   getTasks,
 } from './store/task';
 const store = createStore();
+
 function App() {
-  const state = useSelector((state) => state);
+  const state = useSelector((state) => state.entities);
+  const isLoading = useSelector((state) => state.isLoading);
+  const error = useSelector((state) => state.error);
   const dispatch = useDispatch();
   console.log(state);
 
@@ -25,46 +28,53 @@ function App() {
     dispatch(taskDeleted(taskID));
   };
 
-  return (
-    <div>
-      App
-      <ul>
-        {state.map((item) => {
-          return (
-            <li key={item.id}>
-              <p>{` ${item.title}`}</p>
-              <p>{`completed: ${item.completed}`}</p>
-              <button
-                className="btn btn-primary m-2"
-                onClick={() => {
-                  dispatch(completeTask(item.id));
-                }}
-              >
-                Complete
-              </button>
-              <button
-                className="btn btn-success m-2"
-                onClick={() => {
-                  changeTitle(item.id);
-                }}
-              >
-                Change title
-              </button>
-              <button
-                className="btn btn-danger m-2"
-                onClick={() => {
-                  deleteTask(item.id);
-                }}
-              >
-                Delete task
-              </button>
-              <hr />
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+  if (isLoading === true) {
+    return <h1>Loading</h1>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  } else if (isLoading !== true) {
+    return (
+      <div>
+        App
+        <ul>
+          {state.map((item) => {
+            return (
+              <li key={item.id}>
+                <p>{` ${item.title}`}</p>
+                <p>{`completed: ${item.completed}`}</p>
+                <button
+                  className="btn btn-primary m-2"
+                  onClick={() => {
+                    dispatch(completeTask(item.id));
+                  }}
+                >
+                  Complete
+                </button>
+                <button
+                  className="btn btn-success m-2"
+                  onClick={() => {
+                    changeTitle(item.id);
+                  }}
+                >
+                  Change title
+                </button>
+                <button
+                  className="btn btn-danger m-2"
+                  onClick={() => {
+                    deleteTask(item.id);
+                  }}
+                >
+                  Delete task
+                </button>
+                <hr />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
